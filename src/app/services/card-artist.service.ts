072bf513c235462d1527a31e79c4eaf1648b1605
LucLry <lucleroy78@gmail.com>
2023-06-09 13:36:46 +0200

    cards artits and service file

---
+++ src/app/services/card-artist.service.ts
@@ -0,0 +1,17 @@
+import { HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CardArtistService {
+  constructor(private http: HttpClient) {}
+
+  getArtistById(ArtistIds: string): Observable<any> {
+    return this.http.get<any>(`http://localhost:8080/getArtist`, {
+      params: new HttpParams().set('spotifyArtistIdList', ArtistIds),
+    });
+  }
+}
